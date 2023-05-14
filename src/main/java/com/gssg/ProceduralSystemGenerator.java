1bb6ed6c80aff1d2fd096c5d9143063d62583b87
k3ssdev <albertoperezdelrio@gmail.com>
2023-05-14 16:25:56 +0200

    update

--- src/main/java/com/gssg/ProceduralSystemGenerator.java
+++ src/main/java/com/gssg/ProceduralSystemGenerator.java
@@ -13,7 +13,7 @@ public class ProceduralSystemGenerator {
     public static void main(String[] args) {
         List<StarData> starDataList = new ArrayList<>();
 
-        // Datos de la estrella 1
+        // Data from Gaia DR3 for star 4295806720
         starDataList.add(new StarData("1636042515805110273", "Gaia EDR3 4295806720", "4295806720", "1067963836", 2016.0,
                 44.99615537864534, 0.10161827, 0.005615226341865997, 0.10133387, 0.3543305595550248, 0.12266381,
                 2.8886316, 12.616485, 11.93835156938502, 0.13794228, -4.0806193394130865, 0.13316983, 0.12293493,
@@ -26,19 +26,18 @@ public class ProceduralSystemGenerator {
                 1.1689758, 0, 0, 0, 0, 0, 0, 176.94278852482034, -48.88493355232444, 42.54657309907107,
                 -16.317212317623884));
 
-        // Genera los sistemas estelares utilizando los datos del catálogo Gaia
+        // Generate star systems from Gaia DR3 data
         List<StarSystem> systems = generateSystems(starDataList);
 
-        // Imprime los sistemas estelares generados
+        // Print star systems
         for (StarSystem system : systems) {
-            //System.out.println(system);
+            // System.out.println(system);
 
-            // Crear un objeto ObjectMapper
+            // ObjectMapper to convert objects to JSON
             ObjectMapper objectMapper = new ObjectMapper();
 
-            // Convertir el objeto a formato JSON
             try {
-                // Imprime el sistema estelar en formato JSON
+                // Pretty print as JSON
                 String jsonOutput = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(system);
                 System.out.println("StarSystem" + jsonOutput);
             } catch (JsonProcessingException e) {
@@ -47,14 +46,14 @@ public class ProceduralSystemGenerator {
         }
     }
 
-    // Genera los sistemas estelares utilizando los datos del catálogo Gaia
+    // Generates a list of star systems from a list of star data
     private static List<StarSystem> generateSystems(List<StarData> starDataList) {
         List<StarSystem> systems = new ArrayList<>();
-        // Genera un sistema estelar por cada estrella del catálogo Gaia
+        // Generate a star system for each star data object
         for (StarData starData : starDataList) {
 
             /*
-             * Crea una estrella con los siguientes atributos:
+             * Atributes of a starData object:
              * designation: The name or identifier of the star.
              * ra: Right ascension, which represents the star's position on the celestial
              * sphere.
@@ -80,22 +79,22 @@ public class ProceduralSystemGenerator {
             star.setYearLight();
             StarSystem system = new StarSystem(starData.getDesignation(), star);
 
-            // Genera un número aleatorio de planetas (hasta 20)
-            int numPlanets = (int) (Math.random() * 20); // Genera un número aleatorio de planetas (hasta 20)
+            // Generate a random number of planets for the star system up to 20
+            int numPlanets = (int) (Math.random() * 20);
             for (int i = 0; i < numPlanets; i++) {
                 Planet planet = generatePlanet(system, i + 1);
                 system.addPlanet(planet);
             }
-            // Añade el sistema estelar a la lista de sistemas
+            // Add the star system to the list of systems
             systems.add(system);
         }
 
         return systems;
     }
 
-    // Genera un planeta con atributos aleatorios
+    // Generate a planet for a star system
     private static Planet generatePlanet(StarSystem starSystem, int planetIndex) {
-        // Genera atributos aleatorios para el planeta
+        // Planet attributes
         String name = generatePlanetName(starSystem, planetIndex);
         double mass = Math.random() * 100;
         double radius = Math.random() * 10;
@@ -103,26 +102,23 @@ public class ProceduralSystemGenerator {
         double orbitalPeriod = Math.random() * 1000;
 
         /*
-         * name: Es una cadena de texto que representa el nombre del planeta, por lo que
-         * no se aplica una unidad específica.
-         * mass: Se refiere a la masa del planeta. Las unidades comunes para la masa
-         * planetaria son masas terrestres (M⊕) o masas solares (M☉).
-         * radius: Representa el radio del planeta. Las unidades comunes para el radio
-         * planetario son radios terrestres (R⊕) o radios solares (R☉).
-         * distance: Indica la distancia del planeta a su estrella. Las unidades comunes
-         * para la distancia planetaria son unidades astronómicas (UA) o kilómetros
-         * (km).
-         * orbitalPeriod: Es el período orbital del planeta, es decir, el tiempo que
-         * tarda en completar una órbita alrededor de su estrella. Las unidades comunes
-         * para el período orbital son días, años terrestres (yr) o cualquier unidad de
-         * tiempo.
+         * name: It is a string that represents the name of the planet, so no specific
+         * unit applies.
+         * mass: Refers to the mass of the planet. Common units for planetary mass are
+         * Earth masses (M⊕) or solar masses (M☉).
+         * radius: Represents the radius of the planet. Common units for planetary
+         * radius are Earth radii (R⊕) or solar radii (R☉).
+         * distance: Indicates the distance of the planet to its star. Common units for
+         * planetary distance are astronomical units (AU) or kilometers (km).
+         * orbitalPeriod: It is the orbital period of the planet, i.e., the time it
+         * takes to complete one orbit around its star. Common units for orbital period
+         * are days, Earth years (yr), or any time unit.
          */
 
         return new Planet(name, mass, radius, distance, orbitalPeriod);
     }
 
-    // Genera el nombre de un planeta a partir del nombre del sistema estelar y el
-    // índice del planeta
+    // Generate a name for a planet in a star system
     private static String generatePlanetName(StarSystem starSystem, int planetIndex) {
         String systemIdentifier = starSystem.getName().replaceAll(" ", "_");
         String planetIdentifier = "P" + planetIndex;
--- src/main/java/com/gssg/Star.java
+++ src/main/java/com/gssg/Star.java
@@ -13,7 +13,6 @@ public class Star {
     private double dr2RadialVelocity;
     private double bpRp;
 
-    // Otros atributos relevantes de una estrella
 
     // Constructor
     public Star(String name, double ra, double dec, double parsecs, double yearLight) {
@@ -37,7 +36,7 @@ public class Star {
         this.bpRp = bpRp;
     }
 
-    // Getters y Setters
+    // Getters and Setters
 
     public String getName() {
         return name;
@@ -96,16 +95,17 @@ public class Star {
     }
 
     /*
-     * Distancia en parsecs (d) = 1 / paralaje (p)
-     * Distancia en años luz (d_ly) = d * 3.26
+     * Distance in parsecs (d) = 1 / parallax (p)
+     * Distance in light-years (d_ly) = d * 3.26
      * 
      * 
-     * La paralaje (p) se mide en segundos de arco.
-     * La distancia (d) se expresa en parsecs.
-     * La distancia en años luz (d_ly) se calcula multiplicando la distancia en
-     * parsecs por el factor de conversión de aproximadamente 3.26.
+     * Parallax (p) is measured in arcseconds.
+     * Distance (d) is expressed in parsecs.
+     * Distance in light-years (d_ly) is calculated by multiplying the distance in
+     * parsecs
+     * by the conversion factor of approximately 3.26.
      */
-    
+
     public void setParsecs() {
         this.parsecs = 1 / this.parallax;
     }
@@ -141,7 +141,8 @@ public class Star {
     @Override
 
     public String toString() {
-        return "Star [name=" + name + ", ra=" + ra + ", dec=" + dec + ", parsecs=" + parsecs + ", y/l=" + yearLight + ", parallax="
+        return "Star [name=" + name + ", ra=" + ra + ", dec=" + dec + ", parsecs=" + parsecs + ", y/l=" + yearLight
+                + ", parallax="
                 + parallax + ", l=" + l + ", b=" + b + ", photGMeanMag=" + photGMeanMag + ", dr2RadialVelocity="
                 + dr2RadialVelocity + ", bpRp=" + bpRp + "]";
     }
